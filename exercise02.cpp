be35734d68905ad0dcf96ff5ef381a8cc5f13d22
it21378034 <99179743+it21378034@users.noreply.github.com>
2022-02-22 17:30:17 +0000

    completed

--- exercise02.cpp
+++ exercise02.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int volume(int height, int width, int length);
@@ -30,4 +30,36 @@ int main() {
     return 0;
 }
 
-// Implement the Volume() function here
+// Implement the Volume() function here*/
+
+
+#include <iostream>
+using namespace std;
+int volume(int height, int width, int length);
+int main() {
+ int box1Height, box1Width, box1Length;
+ int box2Height, box2Width, box2Length;
+ int totalVolume, totalSurface,volume1,volume2,Surface1,Surface2;
+
+ cout << "Enter Box 1 Height : ";
+ cin >> box1Height;
+ cout << "Enter Box 1 Width : ";
+ cin >> box1Width;
+ cout << "Enter Box 1 Length : ";
+ cin >> box1Length;
+
+ cout << "Enter Box 2 Height : ";
+ cin >> box2Height;
+ cout << "Enter Box 2 Width : ";
+ cin >> box2Width;
+ cout << "Enter Box 2 Length : ";
+ cin >> box2Length;
+
+ totalVolume = (volume1=(box1Height* box1Width* box1Length))+(volume2=(box1Height* box1Width* box1Length));//to calculate the totalvolume
+ totalSurface=(Surface1=(box1Height* box1Width)*6)+(Surface2=(box1Height* box1Width)*6);
+ 
+ cout << "Volume of Box is " << totalVolume << endl;//print the total volume
+  cout << "Surface of Box is " << totalVolume << endl;//print the total Surface
+
+ return 0;
+}
